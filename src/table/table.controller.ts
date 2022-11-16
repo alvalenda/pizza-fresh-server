d90af2c1063097b5f8458b35e40030a8812cf43b
alvalenda <banysan@msn.com>
2022-11-16 06:37:12 -0300

    feat(table): create Post route

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('table')
 export class TableController {
   @Get()
   findAll(): string {
     return 'This action returns all tables';
+  }
+
+  @Post()
+  create(): string {
+    return 'This action adds a new table';
   }
 }
