3278dee6a2a8a1a7d4703b1dda235f9044498403
alvalenda <banysan@msn.com>
2022-11-16 08:09:22 -0300

    docs: implement swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '$/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Validation
+  // app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('PizzaFresh')
+    .setDescription('Aplicação para gestão das mesas de uma pizzaria')
+    .setVersion('1.0.0')
+    .addTag('status')
+    .addTag('table')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // http://localhost:3000/api
+
   await app.listen(3333);
 }
+
 bootstrap();
--- src/table/dto/create-table.dto.ts
+++ src/table/dto/create-table.dto.ts
@@ -1,3 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+// import { IsNumber } from "class-validator";
+
 export class CreateTableDto {
+  // @IsNumber()
+  @ApiProperty({
+    description: 'O número da mesa',
+    example: 1,
+  })
   number: number;
 }
