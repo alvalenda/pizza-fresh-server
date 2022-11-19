d1724623793a92d60299901680ea6fd8ae3e77b0
alvalenda <banysan@msn.com>
2022-11-19 16:21:08 -0300

    feat(product): implement `dto` & `service`

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { PrismaModule } from '$/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule], // <--- Add this line to import the service
   controllers: [ProductController],
-  providers: [ProductService]
+  providers: [ProductService],
 })
 export class ProductModule {}
